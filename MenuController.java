09267464a62ae7b28a8d9fa76ad4ceee1d0345f9
Demesmaecker Daniel <d.demesmaecker@gmail.com>
2017-10-24 15:09:39 +0200

    addMember.fxml, sample.xml & MenuController

---
+++ MenuController.java
@@ -0,0 +1,80 @@
+package be.kokw.controllers;
+
+
+
+import be.kokw.controllers.books.AddBook;
+import be.kokw.repositories.books.interfaces.BookRepo;
+import javafx.fxml.FXML;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by ufotje on 20/10/2017.
+ * This is the MenuControllerClass
+ */
+@Controller
+public class MenuController {
+    private BookRepo repo;
+
+    @Autowired
+    public void setRepo(@Qualifier("bookRepo") BookRepo repo) {
+        this.repo = repo;
+    }
+
+    //BookMethods
+    //Add a Book
+    @FXML
+    private void addBook() {
+        AddBook addBook = new AddBook(repo);
+    }
+
+    //Find a Book
+    @FXML
+    private void findBookByTitle(){}
+    @FXML
+    private void findBookByAuthor(){}
+    @FXML
+    private void findBookByPublisher(){}
+
+    //Update a book
+    @FXML
+    private void updateBookByTitle(){}
+    @FXML
+    private void updateBookByAuthor(){}
+    @FXML
+    private void updateBookByPublisher(){}
+
+    //Delete a Book
+    @FXML
+    private void deleteBookByTitle(){}
+    @FXML
+    private void deleteBookByAuthor(){}
+    @FXML
+    private void deleteBookByPublisher(){}
+
+    //MemberMethods
+    //Add a Member
+    @FXML
+    private void addMember(){}
+
+    //Find a Member
+    @FXML
+    private void findMemberByName(){}
+    @FXML
+    private void findMemberByCity(){}
+    @FXML
+    private void findMemberByDayOfBirth(){}
+    @FXML
+    private void findMemberByPayed(){}
+    @FXML
+    private void findMemberByAnalen(){}
+
+    //Update Member
+    @FXML
+    private void updateMember(){}
+
+    //Delete Member
+    @FXML
+    private void deleteMember(){}
+}
